db3074e4d62adef5036e7d32e097eaaf720590a5
tiagoperes <tiagoperesfr@gmail.com>
2017-09-28 17:06:52 +0000

    2

--- script.js
+++ script.js
@@ -15,7 +15,29 @@
     });
   }
   
+  function createCollapseButtons() {
+    $('h2').click(function () {
+      var section = $(this).closest('section'); 
+      if (section.hasClass('collapsed')) section.removeClass('collapsed'); 
+      else section.addClass('collapsed');
+    });
+  }
+  
+  function collapsePastSections() {
+    var collapsible = $('[data-collapse-after]');
+    for (let i = 0; i < collapsible.length; i++) {
+      let date = new Date($(collapsible[i]).attr('data-collapse-after'));
+      if (date.getTime() < new Date().getTime()) {
+        $(collapsible[i]).addClass('collapsed');
+      }
+    }
+    $(window).scrollTop($('section:not(.collapsed)').first().position().top);
+  }
+  
   createMoreInfoButtons();
   createCloseButtons();
+  createCollapseButtons();
+  collapsePastSections();
+  addToHomescreen();
   
 }());
